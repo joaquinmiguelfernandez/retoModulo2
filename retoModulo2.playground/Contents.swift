765e8064d3cc35f15666ceaff2843f1d0515eacb
G&D Iberica <gdiberica@Demos-MacBook-Air.local>
2017-10-03 12:51:02 +0200

    Upload the solution for Module 2 challenge

---
+++ retoModulo2.playground/Contents.swift
@@ -0,0 +1,23 @@
+// Reto modulo 2
+
+import UIKit
+
+var numeros = 0...100
+
+for numero in numeros {
+    
+    if numero % 2 == 0 {
+        print("# \(numero) par!!!")
+    } else {
+        print("# \(numero) impar!!!")
+    }
+    
+    if numero % 5 == 0 {
+        print("# \(numero) Bingo!!!")
+    }
+    
+    if numero > 30 && numero < 40 {
+        print("# \(numero) Viva Swift!!!")
+    }
+
+}
